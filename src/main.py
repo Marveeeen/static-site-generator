8677dfed47ebaa8a07b36caee6dea4e24714118d
Marveeeen <jmvroqueee@gmail.com>
2025-07-17 18:33:09 +0800

    add new utility functions

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from textnode import TextNode, TextType
+from inline_markdown import split_nodes_image
 
 
 def main():
-    node = TextNode("This is text with a `code block` word", TextType.TEXT)
-
-    print(new_nodes)
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    print(split_nodes_image([node]))
+    print(node)
   
 if __name__ == "__main__":
     main()
