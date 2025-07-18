6d29c4686a84d8bccea8a6af1ea33f3f7f160449
Marveeeen <jmvroqueee@gmail.com>
2025-07-18 18:12:47 +0800

    add copy static helper

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
-from textnode import TextNode, TextType
-from inline_markdown import split_nodes_image
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main():
-    node = TextNode(
-        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-        TextType.TEXT,
-    )
-    print(split_nodes_image([node]))
-    print(node)
-  
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+
 if __name__ == "__main__":
     main()
